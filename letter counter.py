cf48b94f432a619eaa59e9b7a3d68f73f4260d86
UddhavSethi <138117178+UddhavSethi@users.noreply.github.com>
2023-12-20 18:13:50 +0530

    letter counter.py

--- letter counter.py
+++ letter counter.py
@@ -1,15 +1,12 @@
-
-
-string="chennai,Bangalore,gurgaon"
-string2="vit,kit,iit"
-x=string.split(",")
-y=string2.split(",")
-dict1={}
-for i in x:
-    for j in y:
-        dict1[i]=j 
-print(dict1)
+word=input("enter word:")
+char=input("enter char:")
+count=0
+while count<=len(word):
+    for char in word:
+        if char==word[count]:
+            count+=1
+print(count)
+            
         
-    
-
+        
     
